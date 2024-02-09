77447cd382fe8c21cf44cdf0f2807f62b1e5a5e2
20HecSan <hectorsanchiii@gmail.com>
2024-02-09 17:18:15 -0500

    almost done

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,15 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    category_name: {
+    category_name:{
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    id:{
+type: DataTypes.INTEGER,
+allowNull: false,
+autoIncrement: true,
+primaryKey: true,
+
     },
   },
   {
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,39 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    id:{
+type: DataTypes.INTEGER,
+allowNull: false,
+primaryKey: true,
+autoIncrement: true
     },
     product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+type: DataTypes.STRING,
+allowNull: false
     },
+    
     price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
+      type: DataTypes.DECIMAL(11,3),
+allowNull: false,
+
     },
     stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
+type: DataTypes.INTEGER,
+allowNull: false,
+defaultValue: 10,
+validate: {
+isNumeric: true
+
+}
     },
-    category_id: {
+    category_id:{
       type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'category',
-        key: 'id',
-      },
-    },
+      allowNull:true,
+      refrences:{
+        model: "category",
+        key: "id",
+      }
+    }
+    
   },
   {
     sequelize,
